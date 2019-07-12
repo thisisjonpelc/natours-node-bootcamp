02fe2e285119858be521914d55e646dd511295ff
Jon Pelc <thisisjonpelc@gmail.com>
2019-07-12 10:32:34 -0500

    Added aggregation pipelines to query for specific tour info

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,7 +5,9 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  aliasTopTours
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan
 } = require('./../controllers/tourController');
 
 const router = express.Router();
@@ -13,6 +15,9 @@ const router = express.Router();
 //router.param('id', checkID);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router
   .route('/')
